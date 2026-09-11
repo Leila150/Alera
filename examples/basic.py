ab61d9e764aa88e1f526c066ae440af2972a6a04
Leila150 <esguerradominguita@gmail.com>
2026-09-11 14:25:17 +0800

    Delete old pyfiler contents

--- examples/basic.py
+++
@@ -1,7 +0,0 @@
-from pyfiler import create_folder, create_file, get_contents, add_contents, folder_contents
-
-create_folder("example_data")
-create_file("example_data/hello.txt", "Hello from pyfiler!\n")
-add_contents("example_data/hello.txt", "Appended text.\n")
-print(get_contents("example_data/hello.txt"))
-print(folder_contents("example_data"))
