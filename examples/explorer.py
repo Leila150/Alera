9ccbf21ee83f7b7581d60c1a30743e9f4a19c617
Leila150 <esguerradominguita@gmail.com>
2026-09-11 14:25:20 +0800

    Delete old pyfiler contents

--- examples/explorer.py
+++
@@ -1,7 +0,0 @@
-from pyfiler import Explorer
-
-fs = Explorer("project_data", create=True)
-fs.create_folder("src")
-fs.create_file("src/main.py", "print('Hello')")
-print(fs.get_contents("src/main.py"))
-print(fs.folder_contents("src"))
