88617c9b6fc7611d036faf448a6852a9d9d60c63
Leila150 <esguerradominguita@gmail.com>
2026-09-11 14:25:24 +0800

    Delete old pyfiler contents

--- examples/metadata.py
+++
@@ -1,6 +0,0 @@
-from pyfiler import create_file, metadata, hash_file, tree
-
-create_file("metadata_demo.txt", "pyfiler")
-print(metadata("metadata_demo.txt"))
-print(hash_file("metadata_demo.txt"))
-print(tree("."))
