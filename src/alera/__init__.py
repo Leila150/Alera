e29e81144f369b32cb6b3324a4f423b94eb300a8
Leila150 <esguerradominguita@gmail.com>
2026-09-12 15:22:48 +0800

    Remove recycle-bin exception export

--- src/alera/__init__.py
+++ src/alera/__init__.py
@@ -2,7 +2,7 @@
 
 from .device import cpu_information, gpu_available, gpu_information, ram_information, spec_information, storage_information
 from .environment import environment_variables, module_available, platform_information, python_information, restrictions
-from .exceptions import AleraBinError, AleraError, AleraPathError, AleraValidationError
+from .exceptions import AleraError, AleraPathError, AleraValidationError
 from .explorer import FileExplorer
 
 __all__ = [
@@ -10,7 +10,6 @@ __all__ = [
     "AleraError",
     "AleraValidationError",
     "AleraPathError",
-    "AleraBinError",
     "cpu_information",
     "gpu_information",
     "gpu_available",
