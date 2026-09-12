ab90681b81fa07dce6fc1d47be741d2732045cf6
Leila150 <esguerradominguita@gmail.com>
2026-09-12 13:26:26 +0800

    Delete examples/searching.py

--- examples/searching.py
+++
@@ -1,7 +0,0 @@
-from pyfiler import search_contents, find_extension
-
-for path in search_contents(".", "pyfiler"):
-    print("content match:", path)
-
-for path in find_extension(".", ".py"):
-    print("Python file:", path)
