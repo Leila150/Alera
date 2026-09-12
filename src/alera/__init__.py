b3825a7629b674bcacba81ce2c5cf4a67a129790
Leila150 <esguerradominguita@gmail.com>
2026-09-12 16:57:20 +0800

    Export new Alera services

--- src/alera/__init__.py
+++ src/alera/__init__.py
@@ -1,13 +1,19 @@
 """Alera: a powerful Python filesystem and system toolkit."""
 
 from .analysis import FileAnalysis
+from .analytics import FilesystemAnalytics
 from .android import AndroidStorage
 from .archive import ArchiveManager
 from .atomic import AtomicFiles
 from .backup import BackupManager
 from .cache import FileCache
+from .calculator import StorageCalculator
+from .cleanup import CleanupManager
+from .core import Alera
 from .device import cpu_information, gpu_available, gpu_information, ram_information, spec_information, storage_information
 from .directory import DirectoryTools
+from .disks import DiskManager
+from .encryption import EncryptionManager
 from .environment import environment_variables, module_available, platform_information, python_information, restrictions
 from .exceptions import AleraBinError, AleraError, AleraPathError, AleraValidationError
 from .explorer import FileExplorer
@@ -15,32 +21,42 @@ from .experimental import Experimental
 from .health import HealthChecker
 from .hidden import HiddenFiles
 from .hidden_explorer import HiddenFileExplorer
+from .inspector import FileInspector
+from .integrity import IntegrityManager
 from .links import LinkManager
 from .locking import FileLock
 from .metadata import MetadataManager
 from .mounts import MountManager
+from .network import NetworkManager
 from .paths import PathTools
 from .permissions import PermissionTools
+from .processes import ProcessManager
 from .recovery import RecoveryManager
 from .search_engine import SearchEngine
 from .security import FileSecurity
 from .snapshots import SnapshotManager
 from .storage import StorageAnalyzer
 from .streams import StreamTools
+from .sync import SyncManager
 from .temporary import TemporaryFiles
 from .transactions import FileTransaction
+from .utilities import FileUtilities
+from .vfs import VirtualFileSystem
+from .versions import VersionManager
 from .watcher import FileWatcher
+from .database import FileDatabase
 
 __all__ = [
-    "FileExplorer", "Experimental", "HiddenFiles", "HiddenFileExplorer", "TemporaryFiles", "ArchiveManager",
-    "FileAnalysis", "AtomicFiles", "FileCache", "DirectoryTools", "FileLock", "PathTools",
-    "PermissionTools", "SnapshotManager", "StreamTools", "FileWatcher", "SearchEngine",
-    "StorageAnalyzer", "MetadataManager", "LinkManager", "BackupManager", "RecoveryManager",
-    "HealthChecker", "FileTransaction", "FileSecurity", "MountManager", "AndroidStorage",
+    "Alera", "FileExplorer", "Experimental", "HiddenFiles", "HiddenFileExplorer", "TemporaryFiles", "ArchiveManager",
+    "FileAnalysis", "AtomicFiles", "FileCache", "DirectoryTools", "FileLock", "PathTools", "PermissionTools",
+    "SnapshotManager", "StreamTools", "FileWatcher", "SearchEngine", "StorageAnalyzer", "MetadataManager",
+    "LinkManager", "BackupManager", "RecoveryManager", "HealthChecker", "FileTransaction", "FileSecurity",
+    "MountManager", "AndroidStorage", "FileInspector", "VirtualFileSystem", "DiskManager", "EncryptionManager",
+    "VersionManager", "SyncManager", "FileUtilities", "CleanupManager", "FileDatabase", "ProcessManager",
+    "NetworkManager", "StorageCalculator", "FilesystemAnalytics", "IntegrityManager",
     "AleraError", "AleraValidationError", "AleraPathError", "AleraBinError",
-    "cpu_information", "gpu_information", "gpu_available", "ram_information",
-    "storage_information", "spec_information", "restrictions", "python_information",
-    "platform_information", "environment_variables", "module_available",
+    "cpu_information", "gpu_information", "gpu_available", "ram_information", "storage_information", "spec_information",
+    "restrictions", "python_information", "platform_information", "environment_variables", "module_available",
 ]
 
 __version__ = "0.1.0"
