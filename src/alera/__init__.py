07c30e34b64f45fed1361fd75400e33175ac7d4b
Leila150 <esguerradominguita@gmail.com>
2026-09-12 15:17:39 +0800

    Expose Alera public API

---
+++ src/alera/__init__.py
@@ -0,0 +1,27 @@
+"""Alera: a powerful Python filesystem and system toolkit."""
+
+from .device import cpu_information, gpu_available, gpu_information, ram_information, spec_information, storage_information
+from .environment import environment_variables, module_available, platform_information, python_information, restrictions
+from .exceptions import AleraBinError, AleraError, AleraPathError, AleraValidationError
+from .explorer import FileExplorer
+
+__all__ = [
+    "FileExplorer",
+    "AleraError",
+    "AleraValidationError",
+    "AleraPathError",
+    "AleraBinError",
+    "cpu_information",
+    "gpu_information",
+    "gpu_available",
+    "ram_information",
+    "storage_information",
+    "spec_information",
+    "restrictions",
+    "python_information",
+    "platform_information",
+    "environment_variables",
+    "module_available",
+]
+
+__version__ = "0.1.0"
