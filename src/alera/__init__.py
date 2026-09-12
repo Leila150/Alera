bf6020db30adef12a3458ca11b30819d2636cd3a
Leila150 <esguerradominguita@gmail.com>
2026-09-12 15:35:25 +0800

    Expand Alera public API to maximum toolkit

--- src/alera/__init__.py
+++ src/alera/__init__.py
@@ -3,33 +3,28 @@
 from .analysis import FileAnalysis
 from .archive import ArchiveManager
 from .atomic import AtomicFiles
+from .cache import FileCache
 from .device import cpu_information, gpu_available, gpu_information, ram_information, spec_information, storage_information
+from .directory import DirectoryTools
 from .environment import environment_variables, module_available, platform_information, python_information, restrictions
 from .exceptions import AleraBinError, AleraError, AleraPathError, AleraValidationError
 from .explorer import FileExplorer
+from .locking import FileLock
+from .paths import PathTools
+from .permissions import PermissionTools
+from .snapshots import SnapshotManager
+from .streams import StreamTools
 from .temporary import TemporaryFiles
+from .watcher import FileWatcher
 
 __all__ = [
-    "FileExplorer",
-    "TemporaryFiles",
-    "ArchiveManager",
-    "FileAnalysis",
-    "AtomicFiles",
-    "AleraError",
-    "AleraValidationError",
-    "AleraPathError",
-    "AleraBinError",
-    "cpu_information",
-    "gpu_information",
-    "gpu_available",
-    "ram_information",
-    "storage_information",
-    "spec_information",
-    "restrictions",
-    "python_information",
-    "platform_information",
-    "environment_variables",
-    "module_available",
+    "FileExplorer", "TemporaryFiles", "ArchiveManager", "FileAnalysis", "AtomicFiles",
+    "FileCache", "DirectoryTools", "FileLock", "PathTools", "PermissionTools",
+    "SnapshotManager", "StreamTools", "FileWatcher",
+    "AleraError", "AleraValidationError", "AleraPathError", "AleraBinError",
+    "cpu_information", "gpu_information", "gpu_available", "ram_information",
+    "storage_information", "spec_information", "restrictions", "python_information",
+    "platform_information", "environment_variables", "module_available",
 ]
 
 __version__ = "0.1.0"
