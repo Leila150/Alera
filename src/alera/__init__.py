a2c36be4f45bc5f49c832df7b8681ed5d0a85ecc
Leila150 <esguerradominguita@gmail.com>
2026-09-12 15:40:39 +0800

    Export HiddenFiles and HiddenFileExplorer

--- src/alera/__init__.py
+++ src/alera/__init__.py
@@ -9,6 +9,8 @@ from .directory import DirectoryTools
 from .environment import environment_variables, module_available, platform_information, python_information, restrictions
 from .exceptions import AleraBinError, AleraError, AleraPathError, AleraValidationError
 from .explorer import FileExplorer
+from .hidden import HiddenFiles
+from .hidden_explorer import HiddenFileExplorer
 from .locking import FileLock
 from .paths import PathTools
 from .permissions import PermissionTools
@@ -18,9 +20,9 @@ from .temporary import TemporaryFiles
 from .watcher import FileWatcher
 
 __all__ = [
-    "FileExplorer", "TemporaryFiles", "ArchiveManager", "FileAnalysis", "AtomicFiles",
-    "FileCache", "DirectoryTools", "FileLock", "PathTools", "PermissionTools",
-    "SnapshotManager", "StreamTools", "FileWatcher",
+    "FileExplorer", "HiddenFiles", "HiddenFileExplorer", "TemporaryFiles", "ArchiveManager",
+    "FileAnalysis", "AtomicFiles", "FileCache", "DirectoryTools", "FileLock", "PathTools",
+    "PermissionTools", "SnapshotManager", "StreamTools", "FileWatcher",
     "AleraError", "AleraValidationError", "AleraPathError", "AleraBinError",
     "cpu_information", "gpu_information", "gpu_available", "ram_information",
     "storage_information", "spec_information", "restrictions", "python_information",
