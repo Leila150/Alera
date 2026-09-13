e6ccc59caac37544bcfafa96aa8c4d5e4c29d34e
Leila150 <esguerradominguita@gmail.com>
2026-09-13 13:12:59 +0800

    Expose Alera internal storage and operation engine

--- src/alera/__init__.py
+++ src/alera/__init__.py
@@ -25,17 +25,19 @@ from .hidden import HiddenFiles
 from .hidden_explorer import HiddenFileExplorer
 from .inspector import FileInspector
 from .integrity import IntegrityManager
+from .internal import AleraStorage
 from .links import LinkManager
 from .locking import FileLock
 from .metadata import MetadataManager
 from .mounts import MountManager
 from .network import NetworkManager
+from .operations import FileEvent, OperationEngine
 from .paths import PathTools
 from .permissions import PermissionTools
 from .processes import ProcessManager
 from .recovery import RecoveryManager
 from .recycle_bin import RecycleBin
-from .search_engine import SearchEngine
+from .search_engine import SearchEngine, SearchResult
 from .search_index import SearchIndex
 from .security import FileSecurity
 from .snapshots import SnapshotManager
@@ -50,16 +52,17 @@ from .versions import VersionManager
 from .watcher import FileWatcher
 
 __all__ = [
-    "Alera", "FileExplorer", "BinaryFileManager", "Experimental", "HiddenFiles", "HiddenFileExplorer", "TemporaryFiles", "ArchiveManager",
-    "FileAnalysis", "AtomicFiles", "FileCache", "DirectoryTools", "FileLock", "PathTools", "PermissionTools",
-    "SnapshotManager", "StreamTools", "FileWatcher", "SearchEngine", "SearchIndex", "StorageAnalyzer", "MetadataManager",
-    "LinkManager", "BackupManager", "RecoveryManager", "HealthChecker", "FileTransaction", "FileSecurity", "RecycleBin",
+    "Alera", "AleraStorage", "OperationEngine", "FileEvent", "FileExplorer", "BinaryFileManager", "Experimental",
+    "HiddenFiles", "HiddenFileExplorer", "TemporaryFiles", "ArchiveManager", "FileAnalysis", "AtomicFiles",
+    "FileCache", "DirectoryTools", "FileLock", "PathTools", "PermissionTools", "SnapshotManager", "StreamTools",
+    "FileWatcher", "SearchEngine", "SearchResult", "SearchIndex", "StorageAnalyzer", "MetadataManager", "LinkManager",
+    "BackupManager", "RecoveryManager", "HealthChecker", "FileTransaction", "FileSecurity", "RecycleBin",
     "MountManager", "AndroidStorage", "FileInspector", "VirtualFileSystem", "DiskManager", "EncryptionManager",
     "VersionManager", "SyncManager", "FileUtilities", "CleanupManager", "FileDatabase", "ProcessManager",
-    "NetworkManager", "StorageCalculator", "FilesystemAnalytics", "IntegrityManager",
-    "AleraError", "AleraValidationError", "AleraPathError", "AleraBinError",
-    "cpu_information", "gpu_information", "gpu_available", "ram_information", "storage_information", "spec_information",
-    "restrictions", "python_information", "platform_information", "environment_variables", "module_available",
+    "NetworkManager", "StorageCalculator", "FilesystemAnalytics", "IntegrityManager", "AleraError",
+    "AleraValidationError", "AleraPathError", "AleraBinError", "cpu_information", "gpu_information", "gpu_available",
+    "ram_information", "storage_information", "spec_information", "restrictions", "python_information",
+    "platform_information", "environment_variables", "module_available",
 ]
 
 __version__ = "0.4.0"
